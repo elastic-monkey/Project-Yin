2b52a49f80d5b8f42fb0b43763297f1c72a94a7b
Kevin Amorim <kevinamorim4@gmail.com>
2016-04-23 15:45:23 +0100

    Added coroutine and collider to Item

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    public Tags TagToCompare;
+    public int Value;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(TagToCompare.ToString()))
+        {
+            var player = other.GetComponent<PlayerBehavior>();
+            StartCoroutine(ApplyItem(player));
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator ApplyItem(PlayerBehavior player)
+    {
+        AddItemToPlayer(player);
+    }
+
+    private void AddItemToPlayer(PlayerBehavior player)
+    {
+    }
+}
