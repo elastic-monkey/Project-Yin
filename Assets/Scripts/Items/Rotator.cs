3511c0d2b6de7e573d09d25a26bcdee8b4300581
Kevin Amorim <kevinamorim4@gmail.com>
2016-04-23 15:12:45 +0100

    Rotator for Items

---
+++ Assets/Scripts/Items/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    public int RotationXAxis;
+    public int RotationYAxis;
+    public int RotationZAxis;
+
+	void Update () {
+
+        transform.Rotate(new Vector3(RotationXAxis, RotationYAxis, RotationZAxis) * Time.deltaTime);
+
+	}
+}
