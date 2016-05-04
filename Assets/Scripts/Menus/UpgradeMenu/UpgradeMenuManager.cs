66463e055dfcbd217ee599d8f357ae7f7a8668df
ee09063 <ee09063@fe.up.pt>
2016-05-04 14:18:05 +0100

    Upgrade Menu v0.1

---
+++ Assets/Scripts/Menus/UpgradeMenu/UpgradeMenuManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpgradeMenuManager : MenuManager {
+
+	public enum Actions
+	{
+		Upgrade
+	}
+
+	public NavMenu UpgradeMenu;
+
+	private bool _isActive;
+	private PlayerBehavior _player;
+
+	private void Update(){
+		if (Input.GetKeyDown (KeyCode.F1) && !_isActive) {
+			ActivateUpgradeMenu (true);
+		} else if (Input.GetKeyDown (KeyCode.F1) && _isActive) {
+			ActivateUpgradeMenu (false);
+		}
+	}
+
+	private void ActivateUpgradeMenu(bool value){
+		Debug.Log ("Menu is " + value);
+		_isActive = value;
+		PlayerInput.GameplayBlocked = value;
+		Time.timeScale = value ? 0 : 1;
+		UpgradeMenu.SetActive (value);
+	}
+
+	public override void OnAction(object action, object data){
+		var actionEnum = (Actions)action;
+
+		switch (actionEnum) {
+			case Actions.Upgrade:
+				Debug.Log ("Upgrade not implemented");
+				break;
+		}
+	}
+}
---
+++ Assets/Scripts/Menus/UpgradeMenu/UpgradeMenuNavItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpgradeMenuNavItem : ButtonNavItem {
+
+	public UpgradeMenuManager.Actions Action;
+
+	public override void OnSelect(MenuManager manager)
+	{
+		manager.OnAction (Action, null);
+	}
+}
