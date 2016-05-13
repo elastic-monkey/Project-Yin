22d0e5d6f81576b73429007b297fa422f60fcf2f
Luís Magalhães <lmaga801@outlook.com>
2016-05-13 13:30:23 +0100

    Refactored Code + Added rubust input verification

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -6,26 +6,46 @@ public abstract class PlayerInput
 
     public static float GetAxis(Axis axis)
     {
+        if (InvalidAxis(axis))
+            return 0;
+        
         return Input.GetAxis(axis.EditorName());
     }
 
     public static float GetAxisRaw(Axis axis)
     {
+        if (InvalidAxis(axis))
+            return 0;
+        
         return Input.GetAxisRaw(axis.EditorName());
     }
 
     public static bool IsButtonDown(Axis axis)
     {
+        if (InvalidAxis(axis))
+            return false;
+
         return Input.GetButtonDown(axis.EditorName());
     }
 
     public static bool IsButtonPressed(Axis axis)
     {
+        if (InvalidAxis(axis))
+            return false;
+        
         return Input.GetButton(axis.EditorName());
     }
 
     public static bool IsButtonUp(Axis axis)
     {
+        if (InvalidAxis(axis))
+            return false;
+        
         return Input.GetButtonUp(axis.EditorName());
+    }
+
+    public static bool InvalidAxis(Axis axis)
+    {
+        return axis == Axis.None;
     }
 }
