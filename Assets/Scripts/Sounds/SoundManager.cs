c6ad733bf0af8f6d8770473b030a27e99b5f3781
Luís Magalhães <lmaga801@outlook.com>
2016-05-28 16:19:51 +0100

    Added Sound manager and Warrior sounds

---
+++ Assets/Scripts/Sounds/SoundManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SoundManager : MonoBehaviour
+{
+    public int MaxAudioSources;
+    public List<AudioSource> Sources;
+
+    private void Awake()
+    {
+        Sources = new List<AudioSource>();
+        for (var i = 0; i < MaxAudioSources; i++)
+        {
+            var obj = new GameObject();
+            obj.name = "Audio Source " + i;
+            obj.transform.SetParent(transform, false);
+            Sources.Add(obj.AddComponent<AudioSource>());
+        }
+    }
+
+    public void Play(AudioClip clip, bool loop)
+    {
+        var ableToPlay = false;
+        foreach (var source in Sources)
+        {
+            if (source.isPlaying)
+                continue;
+
+            ableToPlay = true;
+            source.clip = clip;
+            source.loop = loop;
+            source.Play();
+            break;
+        }
+
+        if (!ableToPlay)
+            Debug.Log("Not able to play sound clip: " + clip.name + ". Consider increasing MaxAudioSources.");
+    }
+        
+    public void Stop(AudioClip clip, bool loop = false)
+    {
+        foreach (var source in Sources)
+        {
+            if ((!loop) || (loop && source.loop))
+            {
+                if (source.clip == clip)
+                    source.Stop();
+            }
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Sounds/WarriorSoundManager.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class WarriorSoundManager : MonoBehaviour
+{
+    public enum ClipActions
+    {
+        Step,
+        WeaponStrike,
+        NULL
+    }
+
+    public SoundManager SoundManager;
+    public bool PlayVariations;
+    public List<AudioClipItem> Clips;
+
+    public void PlayClip(ClipActions action)
+    {
+        Play(action, false);
+    }
+
+    public void Play(ClipActions action, bool loop)
+    {
+        var clip = FindClip(action);
+        if (clip == null)
+            return;
+
+        SoundManager.Play(clip, loop);
+    }
+
+    public void Stop(ClipActions action)
+    {
+        var clip = FindClip(action);
+        if (clip == null)
+            return;
+
+        SoundManager.Stop(clip);
+    }
+
+    private AudioClip FindClip(ClipActions action)
+    {
+        if (PlayVariations)
+        {
+            var index = -1;
+            for (var i = 0; i < Clips.Count; i++)
+            {
+                var clip = Clips[i];
+                if (clip.Action.Equals(action))
+                {
+                    index = (index < 0) ? i : (Random.Range(0, 2) < 1) ? index : i;
+                }
+            }
+
+            if (index >= 0)
+            {
+                return Clips[index].Clip;
+            }
+        }
+        else
+        {
+            foreach (var clip in Clips)
+            {
+                if (clip.Action.Equals(action))
+                    return clip.Clip;
+            }
+        }
+
+        return null;
+    }
+
+    #region Editor Only
+
+    private void OnValidate()
+    {
+        foreach (var item in Clips)
+        {
+            if (item.Clip != null)
+                item.Name = string.Concat(item.Action.ToString(), " --> ", item.Clip.name);
+            else
+                item.Name = item.Action.ToString();
+        }
+    }
+
+    #endregion
+
+    [System.Serializable]
+    public class AudioClipItem
+    {
+        [HideInInspector]
+        public string Name;
+        public ClipActions Action;
+        public AudioClip Clip;
+    }
+}
